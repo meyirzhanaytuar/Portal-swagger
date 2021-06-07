abfbe4e8e8174b6433751c352c1503b93bee4d42
L <ermek.rahimberdiev@gmail.com>
2021-06-07 10:20:48 +0600

    sidebar deviding

--- src/app/consts/routes.ts
+++ src/app/consts/routes.ts
@@ -3,13 +3,8 @@ export enum routes {
     STUDENT_WS = '/student-ws',
     STUDENT_SH = '/student-sh',
     STUDENT_PL = '/student-pl',
-    TYPOGRAPHY = '/typography',
-    TABLES = '/tables',
-    NOTIFICATION = '/notification',
-    UI_ELEMENTS_ICONS = '/ui/icons',
-    UI_ELEMENTS_CHARTS = '/ui/charts',
-    UI_ELEMENTS_MAP = '/ui/map',
-    LOGIN = '/login',
     TEACHER_SH = '/teacher-sh',
+    TEACHER_JOURNAL = '/teacher-journal',
+    LOGIN = '/login',
 
 }
