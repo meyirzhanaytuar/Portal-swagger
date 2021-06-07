7d79f9263a07f2a6c770ea4bfdb8e5e7a61d964a
L <ermek.rahimberdiev@gmail.com>
2021-06-07 10:29:32 +0600

    teacher dashboard menu item

--- src/app/consts/routes.ts
+++ src/app/consts/routes.ts
@@ -1,10 +1,11 @@
 export enum routes {
     DASHBOARD = '/dashboard',
+    LOGIN = '/login',
     STUDENT_WS = '/student-ws',
     STUDENT_SH = '/student-sh',
     STUDENT_PL = '/student-pl',
     TEACHER_SH = '/teacher-sh',
     TEACHER_JOURNAL = '/teacher-journal',
-    LOGIN = '/login',
+    TEACHER_WS = '/teacher-dashboard',
 
 }
