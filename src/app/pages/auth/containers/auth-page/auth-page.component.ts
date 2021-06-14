e859c6b14339cc1703d22dc1eec04c1e7d81012c
meyirzhanaytuar <Meirjan.k1n3@gmail.com>
2021-06-14 09:31:21 +0600

    Authentification and teacher-scheduler

--- src/app/pages/auth/containers/auth-page/auth-page.component.ts
+++ src/app/pages/auth/containers/auth-page/auth-page.component.ts
@@ -1,32 +1,37 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { AuthService } from '../../services';
-import { routes } from '../../../../consts';
+import {AuthService} from '../../services';
+import {routes} from '../../../../consts';
 
 @Component({
-  selector: 'app-auth-page',
-  templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.scss']
+    selector: 'app-auth-page',
+    templateUrl: './auth-page.component.html',
+    styleUrls: ['./auth-page.component.scss']
 })
 export class AuthPageComponent {
-  public todayDate: Date = new Date();
-  public routers: typeof routes = routes;
+    public todayDate: Date = new Date();
+    public routers: typeof routes = routes;
+    public authCred = {
+        login: 'aytuarme',
+        password: '1234',
+    };
 
-  constructor(
-    private service: AuthService,
-    private router: Router
-  ) { }
+    constructor(
+        private service: AuthService,
+        private router: Router
+    ) {
+    }
 
-  public sendLoginForm(): void {
-    this.service.login();
+    public sendLoginForm(): void {
 
-    this.router.navigate([this.routers.DASHBOARD]).then();
-  }
+        this.service.login(this.authCred);
 
-  public sendSignForm(): void {
-    this.service.sign();
+    }
 
-    this.router.navigate([this.routers.DASHBOARD]).then();
-  }
+    public sendSignForm(): void {
+        this.service.sign();
+
+        this.router.navigate([this.routers.DASHBOARD]).then();
+    }
 }
--- src/app/pages/auth/services/auth.service.ts
+++ src/app/pages/auth/services/auth.service.ts
@@ -1,28 +1,49 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
 
-import { User } from '../models';
+import {User} from '../models';
+import {Router} from '@angular/router';
+import {routes} from '../../../consts';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  public login(): void {
-    localStorage.setItem('token', 'token');
-  }
+    public routers: typeof routes = routes;
+    private readonly AUTH_ENDPOINT = '/auth';
 
-  public sign(): void {
-    localStorage.setItem('token', 'token');
-  }
+    constructor(private router: Router,
+                private http: HttpClient) {
+    }
 
-  public signOut(): void {
-    localStorage.removeItem('token');
-  }
+    public login(authCred): void {
+        this.http.post(this.AUTH_ENDPOINT, authCred, {observe: 'response'}).subscribe(res => {
+            console.log(res);
+            console.log(res.headers);
+            console.log(res.headers.get('Authorization'));
+            console.log(res.status);
+            if (res.status === 200) {
+                localStorage.setItem('token', res.headers.get('Authorization'));
+                this.router.navigate([this.routers.DASHBOARD]).then();
 
-  public getUser(): Observable<User> {
-    return of({
-      name: 'John',
-      lastName: 'Smith'
-    });
-  }
+            }
+        });
+
+    }
+
+    public sign(): void {
+        localStorage.setItem('token', 'token');
+    }
+
+    public signOut(): void {
+        localStorage.removeItem('token');
+    }
+
+    public getUser(): Observable<User> {
+        return of({
+            name: 'John',
+            lastName: 'Smith'
+        });
+    }
 }
